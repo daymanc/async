eb87213c319182c760b73b2f63307eb0b5d90d5e
daymanc <dayman@pistoncloud.com>
2017-02-09 13:12:59 -0800

    async_socket_1.py
    Create an asynchronous non-blocking reading socket, Python 3 lib

---
+++ async_socket_1.py
@@ -0,0 +1,66 @@
+from selectors import DefaultSelector, EVENT_WRITE, EVENT_READ
+
+import pdb
+import socket
+import time
+
+"""Create an asynchronous non-blocking reading socket, Python 3 lib"""
+
+selector = DefaultSelector()
+n_jobs = 0
+
+class Future:
+    def __init__(self):
+        self.callback = None
+
+    def resolve(self):
+        self.callback()
+
+def get(path):
+    global n_jobs
+    n_jobs += 1
+    s = socket.socket()
+    s.setblocking(0)
+    try:
+        s.connect(('localhost', 5000))
+    except BlockingIOError:
+        pass
+
+    f = Future()
+    f.callback = lambda: connected(s, path)
+    selector.register(s.fileno(), EVENT_WRITE, f)
+
+def connected(s, path):
+    selector.unregister(s.fileno())
+    s.send(('GET %s HTTP/1.0\r\n\r\n' % path).encode())
+    buf = []
+    f = Future()
+    f.callback = lambda: readable(s, buf)
+    selector.register(s.fileno(), EVENT_READ, f)
+
+def readable(s, buf):
+    global n_jobs
+    selector.unregister(s.fileno())
+    chunk = s.recv(1000)
+    if chunk:
+        buf.append(chunk)
+        f = Future()
+        f.callback = lambda: readable(s, buf)
+        selector.register(s.fileno(), EVENT_READ, f)
+    else:
+        # Finished.
+        s.close()
+        print((b''.join(buf)).decode().split('\n')[0])
+        n_jobs -= 1
+
+start = time.time()
+get('/foo')
+get('/bar')
+
+while n_jobs:
+    events = selector.select()
+    for key, mask in events:
+        future = key.data
+        future.resolve()
+
+print('Data transaction took %.1f seconds' % (time.time() - start))
